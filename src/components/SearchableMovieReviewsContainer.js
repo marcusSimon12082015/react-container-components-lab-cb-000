75458b6be7ff33e84a14c556f2ee5e7223ae7dde
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2019-07-21 16:56:14 +0000

    Automatically backed up by Learn

--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -7,7 +7,7 @@ const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code SearchableMovieReviewsContainer Here
-export default class LatestMovieReviewsContainer extends React.Component{
+export default class SearchableMovieReviewsContainer extends React.Component{
   constructor(){
     super();
     this.state = {
