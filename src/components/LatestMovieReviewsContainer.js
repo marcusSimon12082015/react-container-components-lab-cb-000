be53de20caf224adfe9f5f5bb6c52f61d39ae640
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2019-07-21 16:34:25 +0000

    Automatically backed up by Learn

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -7,3 +7,11 @@ const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code LatestMovieReviewsContainer Here
+class LatestMovieReviewsContainer extends React.Component{
+  constructor(){
+    super();
+    this.state = {
+      reviews:[]
+    }
+  }
+}
