20c04c97916a8e181a1cef92beceb19b41af664b
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2019-07-21 16:48:58 +0000

    Automatically backed up by Learn

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,10 @@
 // Code MovieReviews Here
+export default class MovieReviews{
+  render(){
+    return(<div className='review-list'>
+    <ul>
+      {this.props.reviews.map((review) => <li>{review}</li>)}
+    </ul>
+    </div>);
+  }
+}
