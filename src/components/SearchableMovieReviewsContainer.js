e8c45ef9a5699aa422a94b7de22d963061eef302
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2019-07-21 17:08:04 +0000

    Automatically backed up by Learn

--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -3,7 +3,7 @@ import 'isomorphic-fetch';
 import MovieReviews from './MovieReviews'
 
 const NYT_API_KEY = 'f98593a095b44546bf4073744b540da0';
-const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
+const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code SearchableMovieReviewsContainer Here
@@ -19,7 +19,7 @@ export default class SearchableMovieReviewsContainer extends React.Component{
 
   handleSubmit(event){
     event.preventDefault();
-    fetch(URL+?query=this.state.searchTerm);
+    fetch(URL+'?query='this.state.searchTerm');
     .then(response => response.json())
     .then(reviews => this.setState({ reviews }))
   }
