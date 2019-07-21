088a84d00d63686587220ef56d70080d733e494c
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2019-07-21 16:53:10 +0000

    Automatically backed up by Learn

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -5,7 +5,7 @@ export default class MovieReviews extends React.Component{
   render(){
     return(<div className='review-list'>
     <ul>
-      {this.props.reviews.map((review) => <li>{review.display_title} - {review.mpaa_rating}</li>)}
+      {this.props.reviews.map((review) => <li className='review'>{review.display_title} - {review.mpaa_rating}</li>)}
     </ul>
     </div>);
   }
